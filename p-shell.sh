bf2177b5aaf833399ad29a5637327c8ffae06a90
Benjamin <BenSchZA@users.noreply.github.com>
2019-07-29 11:42:32 +0200

    Create p-shell.sh

---
+++ p-shell.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+
+FILE=$1
+FILE_PATH=$(realpath $FILE)
+DIR_PATH=$(dirname $FILE_PATH)
+BASE=$(basename $FILE_PATH)
+DER_PATH=$DIR_PATH/derivation_$BASE
+
+mkdir -p $DER_PATH/shell-deps/
+
+nix-instantiate $FILE_PATH --indirect --add-root $DER_PATH/shell.drv
+nix-store -r $(nix-store --query --references $DER_PATH/shell.drv) \
+  --indirect --add-root $DER_PATH/shell-deps/shell.dep
